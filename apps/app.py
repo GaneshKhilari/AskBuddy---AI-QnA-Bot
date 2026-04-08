855ff0d68e761e287ed30b5c98c50dcbedb02c30
Ganesh <khilariganesh2003@gmail.com>
2026-04-08 11:37:32 +0530

    Initial commit - AI chatbot using LangChain and Gemini

---
+++ apps/app.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_google_genai import ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# while True:
+#     query = input("User: ")
+#     if(query.lower()) in ["quit","bye","exit"]:
+#         print("GoodBye👋")
+#         break
+#     result =llm.invoke(query)
+#     print("AI: ", result.content)
+
+import streamlit as st
+st.title("🧠 AskBuddy - AI QnA Bot")        #title idea : "Curious? Just ask!"
+st.markdown("My QnA Bot with LangChain and Googl Gemini !")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    role = message["role"]
+    content = message["content"]
+    st.chat_message(role).markdown(content)
+
+query = st.chat_input("Ask anything ?")
+if query :
+    st.session_state.messages.append({"role":"user", "content":query})
+    st.chat_message("user").markdown(query)
+    # res = llm.invoke(query)
+    # st.chat_message("ai").markdown(res.content)
+    # st.session_state.messages.append({"role":"ai", "content":res.content})
+    with st.spinner("Thinking... 🤔"):
+        try:
+            res = llm.invoke(query)
+            response = res.content
+        except Exception as e:
+            response = f"❌ Error: {str(e)}"
+
+    st.chat_message("ai").markdown(response)
+    st.session_state.messages.append({"role":"ai", "content":response})
